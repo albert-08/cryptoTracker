43820e36e90e2ce031f979daf303e28115e17571
Alberto Flores <alberto.floreso94@gmail.com>
2021-04-08 08:28:20 -0500

    add CoinsItem

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Pressable, StyleSheet, FlatList, ActivityIndicator } from 'react-native'
 import Http from 'cryptoTracker/src/libs/http'
+import CoinsItem from './CoinsItem'
+import Colors from 'cryptoTracker/src/res/colors'
 
 class CoinsScreen extends Component {
 
+    state = {
+        coins: [],
+        loading: false
+    }
+
     componentDidMount = async () => {
+        this.setState({ loading: true })
+
         const coins = await Http.instance.get("https://api.coinlore.net/api/tickers/")
+
+        this.setState({ coins: res.data, loading: false })
     }
 
     handlePress = () => {
@@ -15,12 +26,26 @@ class CoinsScreen extends Component {
     }
 
     render() {
+
+        const { coins, loading } = this.state
+
         return(
             <View style={styles.container}>
-                <Text style={styles.titleText}>CoinsScreen</Text>
-                <Pressable style={styles.btn} onPress={this.handlePress}>
-                    <Text style={styles.btnText}>Ir a detail</Text>
-                </Pressable>
+                { loading
+                    ? <ActivityIndicator
+                        style={styles.loader}
+                        color="#fff" 
+                        size="large" 
+                    />
+                    : null
+                }
+
+                <FlatList 
+                    data={coins} 
+                    renderItem={({ item }) => 
+                        <CoinsItem item={item} />  
+                    } 
+                />
             </View>
         )
     }
@@ -29,7 +54,7 @@ class CoinsScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "red"
+        backgroundColor: Colors.charade
     },
     titleText: {
         color: "#fff",
@@ -44,6 +69,9 @@ const styles = StyleSheet.create({
     btnText: {
         color: "#fff",
         textAlign: "center"
+    },
+    loader: {
+        marginTop: 60
     }
 })
 
